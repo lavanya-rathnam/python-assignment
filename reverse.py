7b9c438c768140f0b81ad1333b235ec6122983d3
lavanya <lavanya.rathnam@sagarsoft.in>
2018-11-05 17:45:47 +0530

    hello world

---
+++ reverse.py
@@ -0,0 +1,26 @@
+# Python3 program to accept an amount
+# and count number of notes
+
+# Function to count and print
+# currency notes
+def countCurrency(amount):
+    notes = [2000, 500, 200, 100,
+             50, 20, 10, 5, 1]
+
+    noteCounter = [0, 0, 0, 0, 0,
+                   0, 0, 0, 0]
+
+    print ("Currency Count -> ")
+
+    for i, j in zip(notes, noteCounter):
+        if amount >= i:
+            j = amount // i
+            amount = amount - j * i
+            print (i, " : ", j)
+
+        # Driver code
+
+
+#amount = 868
+amount = int(input("Enter withdrawl amount: "))
+countCurrency(amount)
